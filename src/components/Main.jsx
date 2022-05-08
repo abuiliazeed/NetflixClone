d63e1588ad5a9f58347cd559789fb0915a4f5a19
abuiliazeed <abuiliazeed@gmail.com>
2022-05-08 15:19:29 +0200

    adding sliders

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -14,8 +14,14 @@ function Main() {
       setMovies(response.data.results);
     });
   }, []);
-  console.log(movie);
 
+  const truncateString = (str, num) => {
+    if (str?.length > num) {
+      return str.slice(0, num) + "...";
+    } else {
+      return str;
+    }
+  };
   return (
     <div className="w-full h-[550px] text-white  ">
       <div className="w-full h-full ">
@@ -26,7 +32,7 @@ function Main() {
           alt={movie?.title}
         />
         <div className="absolute w-full top-[20%] p-4 md:p-8">
-            <h1 className="text-3xl md:text-5xl font-bold">{movie?.title}</h1>
+          <h1 className="text-3xl md:text-5xl font-bold">{movie?.title}</h1>
           <div className="my-4">
             <button className="border bg-gray-300 text-black border-gray-300 py-2 px-5">
               Play
@@ -35,7 +41,12 @@ function Main() {
               Watch Later
             </button>
           </div>
-          <p className="text-gray-400 text-sm">Released: {movie?.release_date}</p>
+          <p className="text-gray-400 text-sm">
+            Released: {movie?.release_date}
+          </p>
+          <p className="w-full md:max-w-[70%] lg:max-w-[50%] xl:max-w-[35%] text-gray-200">
+            {truncateString(movie?.overview,150)}
+          </p>
         </div>
       </div>
     </div>
