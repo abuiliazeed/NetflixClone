55b88d5a20ffa32164a6b08db4103b6e140e9eb2
abuiliazeed <abuiliazeed@gmail.com>
2022-05-08 15:37:38 +0200

    created movie component

--- src/components/Row.jsx
+++ src/components/Row.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
+import Movie from "./Movie";
 
 function Row({ title, fetchURL }) {
   const [movies, setMovies] = useState([]);
@@ -15,12 +16,8 @@ function Row({ title, fetchURL }) {
       <div className="relative flex items-center">
         <div id={"slider"}>
           {movies.map((item, id) => (
-            <div className="w-[160px] sm:w-[200px] md:w-[240px] lg:w-[280px] inline-block cursor-pointer relative p-2">
-                <img className="w-full h-auto block" src={`https://image.tmdb.org/t/p/w500/${item?.backdrop_path}`} alt={item?.title}/>
-                <div className="absolute top-0 left-0 w-full h-full hover:bg-black/80 obacity-0 hover:obacity-100 text-white">
-                    <p className="white-space-normal text-xs md:text-sm font-bold flex justify-center items-center h-full text-center">{item?.title}</p>
-                </div>
-            </div>
+              <Movie item={item} key = {id} />
+
           ))}
         </div>
       </div>
