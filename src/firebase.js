48098ab03c37ae6d1bd7dad5c49036ae25dd124c
abuiliazeed <abuiliazeed@gmail.com>
2022-05-08 16:09:58 +0200

    update

---
+++ src/firebase.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCy85vQG36pTu2Qwkox0ZVUurIolEYmwqc",
+  authDomain: "reactnetflixclone-20fd2.firebaseapp.com",
+  projectId: "reactnetflixclone-20fd2",
+  storageBucket: "reactnetflixclone-20fd2.appspot.com",
+  messagingSenderId: "604500421862",
+  appId: "1:604500421862:web:151a43be2c999fe538df27",
+  measurementId: "G-LVM3P0NDS9"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
