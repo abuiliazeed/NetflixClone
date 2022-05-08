c3dd9f34d63f816581f306835715440f9da67dc9
abuiliazeed <abuiliazeed@gmail.com>
2022-05-08 14:28:45 +0200

    added main banner

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
+import Navbar from "./components/Navbar";
+import { Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+
 function App() {
   return (
-    <div >
-    </div>
+    <>
+    <Navbar />
+    <Routes>
+      <Route path="/" element={<Home />} />
+    </Routes>
+    </>
   );
 }
 
